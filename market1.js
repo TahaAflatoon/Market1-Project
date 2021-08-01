6ff579074631d6304d0d1cbe4407c981f04a3c85
TahaAflatoon <82364316+TahaAflatoon@users.noreply.github.com>
2021-08-01 13:56:00 +0430

    some file changes

--- market1.js
+++ market1.js
@@ -14,7 +14,7 @@ jQuery(document).ready(function($) {
             const hash = this.hash;
             $('html, body').animate({
                 scrollTop: $(hash).offset().top - 50
-                },800);
+            },800);
         }
     });
 })
