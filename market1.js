69939144ab3e9dfe9d9b999f4ef533d6482521e8
TahaAflatoon <82364316+TahaAflatoon@users.noreply.github.com>
2021-08-01 13:32:01 +0430

    Market-1 Project

---
+++ market1.js
@@ -0,0 +1,45 @@
+/*JQuery*/
+$.noConflict()
+jQuery(document).ready(function($) {
+    $('.fa-search').click(function() {
+        $('.search-input').toggle(500);
+    }),
+    $('.bars').click(function() {
+        $('.mobile').slideToggle(500);
+    }),
+    //smooth scroll
+    $('.links a, #header').on('click', function(event) {
+        if (this.hash !== '') {
+            event.preventDefault();
+            const hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top - 50
+                },800);
+        }
+    });
+})
+
+/*JavaScript*/
+//Sticky menu background
+window.addEventListener('scroll', function() {
+    if (window.scrollY > 51) {
+        document.querySelector('.header').style.opacity = 0.9;
+    } else {
+        document.querySelector('.header').style.opacity = 1;
+    }
+});
+
+// side navigation menu
+function openNav() {
+    document.getElementById("mySidenav").style.width = "75%";
+}
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+//Go to top button
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+/*-----------*/
