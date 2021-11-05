5d8ab439b72482e55cefac3fb7d605701f713289
TahaAflatoon <82364316+TahaAflatoon@users.noreply.github.com>
2021-11-05 22:57:27 +0330

    improved syntax & readability

--- market1.js
+++ market1.js
@@ -1,15 +1,15 @@
-/*JQuery*/
+// JQuery
 $.noConflict()
 jQuery(document).ready(function($) {
-    //search icon button
+    // Search icon button
     $('.fa-search').click(function() {
         $('.search-input').toggle(500);
     }),
-    //bars button
+    // Bars button
     $('.bars').click(function() {
         $('.mobile').slideToggle(500);
     }),
-    //smooth scroll
+    // Smooth scroll
     $('.links a, #header').on('click', function(event) {
         if (this.hash !== '') {
             event.preventDefault();
@@ -21,8 +21,8 @@ jQuery(document).ready(function($) {
     });
 })
 
-/*JavaScript*/
-//Sticky menu background
+// JavaScript
+// Sticky menu background
 window.addEventListener('scroll', function() {
     if (window.scrollY > 51) {
         document.querySelector('.header').style.opacity = 0.9;
@@ -31,7 +31,7 @@ window.addEventListener('scroll', function() {
     }
 });
 
-//side navigation menu
+// Side navigation menu
 function openNav() {
     document.getElementById("mySidenav").style.width = "75%";
 }
@@ -39,9 +39,8 @@ function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
 
-//Go to top button
+// Go to top button
 function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
-/*-----------*/
